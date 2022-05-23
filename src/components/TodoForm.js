a9cc533078e4b4341b426c90774d23849c624c0a
Chathura <chathura.madura11@gmail.com>
2022-05-24 00:23:19 +0530

    create TodoForm component

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,10 +1,63 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
-const TodoForm = () => {
+const TodoForm = (props) => {
+    const [input, setInput] = useState(props.edit ? props.edit.value : '');
+
+    const inputRef = useRef(null);
+  
+    useEffect(() => {
+      inputRef.current.focus();
+    });
+  
+    const handleChange = e => {
+      setInput(e.target.value);
+    };
+  
+    const handleSubmit = e => {
+        //to remove refreshing...
+      e.preventDefault();
+    // to parent
+      props.onSubmit({
+        id: Math.floor(Math.random() * 1000),
+        text: input
+      });
+      //to clear after adding to list
+      setInput('');
+    };
+  
     return (
-        <div>
-            
-        </div>
+      <form onSubmit={handleSubmit} className='todo-form'>
+        {props.edit ? (
+          <div>
+            <input
+              placeholder='Update your item'
+              value={input}
+              //when type on input field
+              onChange={handleChange}
+              name='text'
+              ref={inputRef}
+              className='todo-input edit'
+            />
+            <button onClick={handleSubmit} className='todo-button edit'>
+              Update
+            </button>
+          </div>
+        ) : (
+          <div>
+            <input
+              placeholder='Add a todo'
+              value={input}
+              onChange={handleChange}
+              name='text'
+              className='todo-input'
+              ref={inputRef}
+            />
+            <button onClick={handleSubmit} className='todo-button'>
+              Add to List
+            </button>
+          </div>
+        )}
+      </form>
     );
 };
 
