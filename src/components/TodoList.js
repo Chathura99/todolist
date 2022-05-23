e83a3735ef41b8d1feca7e293c2d8a9b117902e4
Chathura <chathura.madura11@gmail.com>
2022-05-24 00:23:32 +0530

    create TodoList component

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,11 +1,63 @@
-import React from 'react';
+import React, { useState } from "react";
+import TodoForm from "./TodoForm";
+import Todo from "./Todo";
 
-const TodoList = () => {
-    return (
-        <div>
-            Hello
-        </div>
+const TodoList = (props) => {
+  // store todo data
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    if (!todo.text) {
+      return;
+    }
+    //concat list
+    const newTodos = [todo, ...todos];
+    setTodos(newTodos);
+    // console.log(...todos);
+  };
+
+  const updateTodo = (todoId, newValue) => {
+    //if update is null, nothing happen
+    if (!newValue.text) {
+      return;
+    }
+    //else
+    setTodos((prev) =>
+      prev.map((item) => (item.id === todoId ? newValue : item))
     );
+  };
+
+  const removeTodo = (id) => {
+      //remove from current list
+    const removedArr = [...todos].filter((todo) => todo.id !== id);
+
+    setTodos(removedArr);
+  };
+
+  const completeTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+          //update all complete to incomplete and incomplete to complete
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div>
+      <h1>Let's Plan {props.title}!</h1>
+      {/* pass "onSubmit" as a props to  TodoForm*/}
+      <TodoForm onSubmit={addTodo} />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
+    </div>
+  );
 };
 
 export default TodoList;
